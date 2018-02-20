88d8160d2a3eca7551683a14727d611b25547a87
G. Ganeva <g.ganeva@gmail.com>
2018-02-20 20:20:22 +0200

    Added empty non-functional projects representing application layers.

---
+++ StudentCourses/StudentCourses.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudentCourses.Web.Startup))]
+namespace StudentCourses.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
